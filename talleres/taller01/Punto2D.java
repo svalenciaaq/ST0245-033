4ada5c299ec25bc523c2413aef1a37e17482ab2b
xltiagoxl <svalenciaa@eafit.edu.co>
2018-01-22 11:51:44 -0500

    Add files via upload

--- talleres/taller01/Punto2D.java
+++ talleres/taller01/Punto2D.java
@@ -10,5 +10,25 @@ package taller1;
  * @author cl18412
  */
 public class Punto2D {
+   
+    private float x;
+     private float y;
+            
     
+    public Punto2D(float x,float y){
+        this.x=x;
+        this.y=y;
+    }
+    
+     public float radio(){
+    return (float) Math.sqrt((x*x)+(y*y));
+    
+    }
+     
+     float angulo(){
+        return (float) Math.atan(y / x);
+       // return (float) Math.atan2(x,y);
+    }
+
 }
+  
