5a52423dcc152812b54ec1569406084ecabfb702
xltiagoxl <svalenciaa@eafit.edu.co>
2018-01-22 21:40:24 -0500

    Add files via upload

---
+++ talleres/taller01/Contador.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller;
+
+/**
+ *
+ * @author usuario
+ */
+public class Contador {
+      String id;
+ static  int contador;
+ static int incrementos;
+    
+  public  Contador(String id){
+        this.id=id;
+        contador=0;
+        incrementos=0;
+    }
+  static  Contador papu= new Contador("Infinito");
+    
+    void Incremetar(){
+        contador+=1;
+        Incrementos();
+        
+    }
+    
+  static int Incrementos(){
+       
+        incrementos+=1;
+        return incrementos;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
+    public static int getIncrementos() {
+        return incrementos;
+    }
+
+    @Override
+    public String toString() {
+        return "Contador{" + "id=" + id + " "+contador+'}';
+    }
+    
+    
+   
+    
+    
+    
+    public static void main(String []args){
+        for (int i = 0; i < 10; i++) {
+            papu.Incremetar();
+        }
+        
+        
+        System.out.println(papu.toString());
+    
+        
+    }
+
+    
+    
+    
+
+   
+}
