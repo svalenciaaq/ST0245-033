8dfda814f964b89525918a9f70a9c7a857acadb6
xltiagoxl <svalenciaa@eafit.edu.co>
2018-01-29 22:54:17 -0500

    Add files via upload

---
+++ talleres/taller02/Punto3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poew;
+
+/**
+ *
+ * @author usuario
+ */
+public class Punto3 {
+    
+    
+    public static void Punto3(String papa, String hijo){
+        if(hijo.length()==0){
+            System.out.println(papa);
+        }else{
+            Punto3(papa+hijo.charAt(0), hijo.substring(1));
+            Punto3(papa,hijo.substring(1));
+        }
+     
+    }
+    
+    public static void main(String[]args){
+        Punto3(" ", "abc");
+    }
+}
